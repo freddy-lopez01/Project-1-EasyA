1b13028df480bcf85e3cf0c74e9db314f1ef9a33
SZSage <simonz@uoregon.edu>
2024-01-24 19:02:19 -0800

    Created a DataFetcher class and refractored the data_fetch() function for better organization. Also included many helper functions

--- graphing/data_fetch.py
+++ graphing/data_fetch.py
@@ -1,58 +1,119 @@
 import sqlite3
+from types import NoneType
 import pandas as pd
 import logging
 from typing import Union
 
-
 # setting up logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-# a dictionary containing user selection
-user_selection = {
-    "graph_type": "single_class", # options: single_class, single_dept, class_level_dept
-    "class_details": {
-        "group_code": "CIS415", # relevant if graph type is single_class
-        "department": "Computer Information Science", # relevant for single_dpt and class_level_dpt
-        "class_level": "415" # relevant if graph type is class_level_dept
-    },
-"instructor_type": "All Instructors", # other option: "Faculty"
-    "grade_type": "Percent As", # other option: "Percents Ds/Fs"
-    "class_count": True
-}
+class DataFetcher:
+    def __init__(self, user_selection: dict, database_path: str):
+        """
+        Initializes the DataFetcher with user's selection and database path.
+
+        Parameters:
+        - user_selection (dict): The user's selection criteria.
+        - database_path (str): The path to the SQLite database.
+        """
+        self.user_selection = user_selection
+        self.database_path = database_path
+        self.connection = None 
+        self.cursor = None
+
+    def connect_to_database(self) -> None:
+        """
+        Establishes a connection to the SQL database and logs success or error.
+        """
+        try:
+            # establish connection to SQL database
+            self.connection = sqlite3.connect(self.database_path)
+            self.cursor = self.connection.cursor()
+            logging.info("---Successfully connected to SQL database---")
+
+        except sqlite3.Error as e:
+            logging.error(f"---Error occured while connecting to database: {e}---")
+            # ensures connection is closed
+            if self.connection:
+                self.connection.close()
+                
 
 
-def fetch_data(user_selection: dict) -> Union[pd.DataFrame, str]:
-    graph_type = user_selection["graph_type"]
-    class_details = user_selection["class_details"]
-    instructor_type = user_selection["instructor_type"]
-    grade_type = user_selection["grade_type"]
-    class_count = user_selection["class_count"]
+    def close_connection(self) -> None:
+        """
+        Closes the SQL database connection.
+        """
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+            logging.info("---SQL database connection closed---")
 
-    # error handling for unsuccessful connection
-    try:
-        # establish connectin to SQL database
-        with sqlite3.connect("../Databases/GradeDatabase.sqlite") as connection:
-            print("-----Connection to SQL database successful-----\n")
-            cur = connection.cursor()
-            group_code = user_selection["class_details"]["group_code"]
-            dataframe = pd.read_sql_query("SELECT * from course_data", connection)
-            # single class 
-            if graph_type == "single_class":
-                # call single_class_query
-                single_class_query(group_code, dataframe)
-            # single department
-                # elif user selects "department"
-                    # call department
 
-            # class-level-department
-                # elif user selects all class levels in that dept
+    def fetch_data(self) -> Union[pd.DataFrame, str]:
+        """
+        Fetches data from the database based on the user's selection.
+        """
+        # make connection with DB 
+        # get user input
+        # if user input == single class
+            # call filter_single_class
+        # elif user_input == single_dept
+            # call filter_single_dept
+        # elif user_input == class_level_dept 
+            # call filter_class_level_dept
+        try:
+            self.connect_to_database()
+            graph_type = self.user_selection["graph_type"]
+            print(f"---graph type: {graph_type}---")
 
-            # close connection
-            return dataframe
+        except sqlite3.Error as e:
+            logging.error(e)
+            return f"An error occurred: {e}"
 
-    except sqlite3.Error as e:
-        logging.error(e)
-        return f"An error occurred: {e}"
+
+    def filter_single_class(self, class_code: str) -> pd.DataFrame:
+        """
+        Filters the DataFrame for rows matching a single class code.
+        """
+        pass
+
+    def filter_single_dept(self, department: str) -> pd.DataFrame:
+        """
+        Filters the DataFrame for rows matching a single department.
+        """
+        pass
+
+    def filter_class_level_dept(self, department: str, class_level: str) -> pd.DataFrame:
+        """
+        Filters the DataFrame for rows matching a department and class level.
+        """
+        pass
+
+    def calc_percent_a(self) -> pd.DataFrame:
+        """
+        Calculate percentages of As by class or department.
+        """
+        pass
+
+    def compare_instructor_type(self) -> pd.DataFrame:
+        """
+        Split data into two DataFrames, one for "All Instructors" and one for "Regular Faculty", and calculate percentage of As for each.
+        """
+        pass
+
+    def easy_As_vs_just_pass(self, grade_type: str) -> pd.DataFrame:
+        """
+        Calculates percentages for two different grade types, "Percent As" and "Percent Ds or Fs" based on user selection.
+        """
+        pass
+
+    def class_count(self) -> pd.DataFrame:
+        """
+        Add count of classes taight by each instructor into DataFrame if user selects this option.
+        """
+        pass
+
 
 
 # functions for each graph type
@@ -63,6 +124,7 @@ def single_class_query(group_code: str, dataframe: pd.DataFrame) -> pd.DataFrame
     Parameters:
     - class_code (str): The code of class to filter by
     - dataframe (pd.DataFrame): The DataFrame to filter
+
 
     Returns:
     - pd.DataFrame: A Pandas DataFrame containing only the rows with the matching class_code.
@@ -89,16 +151,32 @@ def single_class_query(group_code: str, dataframe: pd.DataFrame) -> pd.DataFrame
         # return empty DataFrame if there's an error
         return pd.DataFrame()
 
-
-def single_dept_query():
+def single_detpt_query():
     pass
 
 
 def class_level_dept_query():
     pass
 
+# a dictionary containing user selection
+user_selection = {
+    "graph_type": "single_class", # options: single_class, single_dept, class_level_dept
+    "class_details": {
+        "group_code": "CIS415", # relevant if graph type is single_class
+        "department": "Computer Information Science", # relevant for single_dpt and class_level_dpt
+        "class_level": "415" # relevant if graph type is class_level_dept
+    },
+"instructor_type": "All Instructors", # other option: "Faculty"
+    "grade_type": "Percent As", # other option: "Percents Ds/Fs"
+    "class_count": True
+}
 
-fetch_data(user_selection)
+dest = "../Databases/GradeDatabase.sqlite"
+
+fetch = DataFetcher(user_selection, dest)
+dataframe = fetch.fetch_data()
+
+
 
 
 

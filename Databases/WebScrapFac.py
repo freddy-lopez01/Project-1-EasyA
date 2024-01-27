fe7a037836e95aa21fd0f8437e5378f8ba4c9a61
Daniel Willard <114689080+DJ-Willard@users.noreply.github.com>
2024-01-27 02:04:54 -0800

    database combine for simon upon request

--- Databases/WebScrapFac.py
+++ Databases/WebScrapFac.py
@@ -6,20 +6,27 @@ import random
 from bs4 import BeautifulSoup
 from collections import namedtuple
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
+from urllib3.util.retry import Retry
 
 ''' Change bool as info is needed
 DEBUGING==========================================================================================================================================================='''
 debug_main = True
-#debug function
+# debug function
 debug_Fac = False
-#debug sort
+# debug sort
 debug_sort = False
-#more info if needed
+# more info if needed
 verbose = False
+# run time
+start_time = time.time()
 '''============================================================================================================================================================'''
 
+# Define a namedtuple to store department and faculty data for future use
+DeptFac = namedtuple('DeptFac', ['dept', 'fac'])
+# create an empty list to return the data tuples
+DeptFac_list = []
+# directory for update and write files
+data_directory = "DataFiles"
 
 # Function to create a session with retry mechanism format form stackoverflow
 def create_session():
@@ -29,13 +36,12 @@ def create_session():
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     return session
-
 # Interative function to open link and pull name data
 def Scrape_FacinDept(department_link, session):
-    #sleep added to prevent web denial
+    # sleep added to prevent web denial
     # Make a request for the department link
     # slow time to ensure connection
-    time.sleep(random.uniform(0,1))
+    time.sleep(random.uniform(0, 1))
     try:
         department_response = session.get(department_link)
     except requests.exceptions.RequestException as e:
@@ -50,9 +56,9 @@ def Scrape_FacinDept(department_link, session):
     if department_response.status_code == 200:
         department_soup = BeautifulSoup(department_response.text, 'html.parser')
 
-        # Formated html file code format that has data needed
-        #<div id="facultytextcontainer" class="tab_content">
-		#		<h3>Participating Faculty</h3>
+        # Formatted html file code format that has data needed
+        # <div id="facultytextcontainer" class="tab_content">
+        #       <h3>Participating Faculty</h3>
         #        <p>Lindsay F. Braun, history</p>
         #        <p class="facultylist">Yvonne A. Braun, women’s and gender studies</p>
         #
@@ -67,7 +73,7 @@ def Scrape_FacinDept(department_link, session):
             filtered_faculty_paragraphs = faculty_container.find_all('p', {'class': ['facultylist', None]})
 
             # Exclude paragraphs that contain specific elements
-            filtered_faculty_paragraphs = [p for p in filtered_faculty_paragraphs  if not p.find('em')]
+            filtered_faculty_paragraphs = [p for p in filtered_faculty_paragraphs if not p.find('em')]
 
             # Find any <h3> element to start marking temporary faculty
             participating_header = faculty_container.find('h3')
@@ -83,10 +89,10 @@ def Scrape_FacinDept(department_link, session):
 
             # Iterate over each <p> tag within faculty_container
             for faculty_paragraph in filtered_faculty_paragraphs:
-                #seperate all values not need form scrap
+                # separate all values not need form scrap
                 faculty_name = faculty_paragraph.text.split(',')[0].strip()
 
-                #Check if the previous <h3> header contains "Faculty"
+                # Check if the previous <h3> header contains "Faculty"
                 previous_h3 = faculty_paragraph.find_previous('h3')
                 if previous_h3 and ('Faculty' or 'None' in previous_h3.text):
                     faculty_type = 'P'
@@ -114,7 +120,7 @@ def Scrape_FacinDept(department_link, session):
 
             return sorted_names
 
-# THIS IS ERROR CHECKING _____________________________________________________________________________________________________________________________
+        # THIS IS ERROR CHECKING _____________________________________________________________________________________________________________________________
         else:
             if debug_Fac:
                 print(f"No faculty information found for {department_link} closing connection.")
@@ -123,137 +129,144 @@ def Scrape_FacinDept(department_link, session):
 
     else:
         if debug_Fac:
-            print(f"Failed to retrieve data for {department_link} closing connection. Status Code:", department_response.status_code)
+            print(f"Failed to retrieve data for {department_link} closing connection. Status Code:",
+                  department_response.status_code)
         return None
 
-'''MAIN----------------------------MAIN-------------------------------------------------------------- MAIN--------------------------MAIN'''
+def main():
+    global start_time
+    global DeptFac_list
+    global data_directory
+    # output for
+    print("Connected To Subprocess WEBSCRAP")
+    fileURL_path = os.path.join(data_directory, "url.txt")
+    with open(fileURL_path, 'r') as file:
+        url = file.read().strip()
+    print("URL pulled form File url.txt")
+    # Debug message before making the request
+    if debug_main:
+        print(f"Connecting to: {url}")
 
-# Define a namedtuple to store department and faculty data for future use
-DeptFac = namedtuple('DeptFac', ['dept', 'fac'])
-# create an empty list to return the data tuples
-DeptFac_list = []
-# directory for update and write files
-data_directory = "DataFiles"
+    try:
+        session = create_session()
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        # Handle connection error
+        print(f"Connection error for {url}: {e}")
+        sys.exit()
 
-# url = 'https://web.archive.org/web/20160922054717/http://catalog.uoregon.edu/arts_sciences/'
-fileURL_path = os.path.join(data_directory, "url.txt")
-with open(fileURL_path, 'r') as file:
-    url = file.read().strip()
-# Debug message before making the request
-if debug_main:
-    print(f"Connecting to: {url}")
+    # Debug message after making the request
+    if debug_main:
+        print(f"Status Code: {response.status_code}")
 
-try:
-    session = create_session()
-    response = requests.get(url)
-except requests.exceptions.RequestException as e:
-    # Handle connection error
-    print(f"Connection error for {url}: {e}")
-    sys.exit()
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Must find the element to pull form the website the deptments are in the ntml elements
+        # Found:
+        # <ul id="/art_sciences/" class="nav"
+        # <li><a href="/web/20141107201343/http://catalog.uoregon.edu/arts_sciences/africanstudies/">African Studies</a></li>
+        # Find the <ul> element with class "nav" and id "/arts_sciences/"
+        department_elements = soup.find('ul', {'class': 'nav', 'id': '/arts_sciences/'})
 
+        # Find all <a> tags within department_elements
+        department_elements = department_elements.find_all('a')
+        # closes web connection no longer need data is local now
+        response.close()
 
-# Debug message after making the request
-if debug_main:
-    print(f"Status Code: {response.status_code}")
+        # initialize lists to hold data
+        department_names = []
+        department_links = []
 
-# Check if the request was successful (status code 200)
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Must find the element to pull form the website the deptments are in the ntml elements
-    # Found:
-    #<ul id="/art_sciences/" class="nav"
-    # <li><a href="/web/20141107201343/http://catalog.uoregon.edu/arts_sciences/africanstudies/">African Studies</a></li>
-    # Find the <ul> element with class "nav" and id "/arts_sciences/"
-    department_elements = soup.find('ul', {'class': 'nav', 'id': '/arts_sciences/'})
+        # debug check to see if element is found
+        if verbose:
+            print(f"elements pulled: {department_elements}")
 
-    #Find all <a> tags within department_elements
-    department_elements = department_elements.find_all('a')
-    # closes web connection no longer need data is local now
-    response.close()
+        if department_elements:
+            # Iterate over each <a> tag in the department_elements
+            # track progress info
+            total_departments = len(department_elements)
+            current_department = 0
+            print("Progress: 0.00%")
+            for department_element in department_elements:
+                # Increase the current_department count
+                current_department += 1
+                # Retrieve the text content of the current <a> tag and append it to the departments list
+                department_name = department_element.text
+                # Retrieve the value of the href attribute of the current <a> tag
+                department_link = "https://web.archive.org/" + department_element.get('href')
+                # try to Call the function to scrape additional data for each department link broken up for clarity
+                try:
+                    faculty_names = Scrape_FacinDept(department_link, session)
+                except requests.exceptions.RequestException as e:
+                    # Handle connection error
+                    print(f"Connection error for {department_link}: {e}")
+                    faculty_names = None
+                finally:
+                    # Close the session to release resources
+                    if 'session' in locals():
+                        session.close()
 
-    #initialize lists to hold data
-    department_names = []
-    department_links = []
+                # Print progress
+                progress = (current_department / total_departments) * 100
+                print(f"Progress: {progress:.2f}%")
 
-    # debug check to see if element is found
-    if verbose:
-        print(f"elements pulled: {department_elements}")
+                # Do something with the returned data, for example, store it in a list
+                if faculty_names:
+                    # Store data in list if data was pulled else emtpy string
+                    DeptFac_list.append(DeptFac(department_name, faculty_names))
+                elif faculty_names is None:
+                    DeptFac_list.append(DeptFac(department_name, ''))
+                    print(f"Missing data due to connection failure, please run again: {department_name}")
 
-    if department_elements:
-        #Iterate over each <a> tag in the department_elements
-        #track progress info
-        total_departments = len(department_elements)
-        current_department = 0
-        for department_element in department_elements:
-            # Increase the current_department count
-            current_department += 1
-            #Retrieve the text content of the current <a> tag and append it to the departments list
-            department_name = department_element.text
-            # Retrieve the value of the href attribute of the current <a> tag
-            department_link =  "https://web.archive.org/" + department_element.get('href')
-            # try to Call the function to scrape additional data for each department link broken up for clarity
-            try:
-                faculty_names = Scrape_FacinDept(department_link, session)
-            except requests.exceptions.RequestException as e:
-                # Handle connection error
-                print(f"Connection error for {department_link}: {e}")
-                faculty_names = None
-            finally:
-                # Close the session to release resources
-                if 'session' in locals():
-                    session.close()
+                # debug indiviual data types Saved if need
+                if verbose:
+                    print("you may need to toubleshoot each data type look to line 116")
+                    department_links.append(department_link)
 
-            # Print progress
-            progress = (current_department / total_departments) * 100
-            print(f"Progress: {progress:.2f}%")
-
-            # Do something with the returned data, for example, store it in a list
-            if faculty_names:
-            #Store data in list if data was pulled else emtpy string
-                DeptFac_list.append(DeptFac(department_name, faculty_names))
-            elif faculty_names is None:
-                DeptFac_list.append(DeptFac(department_name, ''))
-                print(f"Missing data due to connection failure, please run again: {department_name}")
-
-            #debug indiviual data types Saved if need
-            if verbose:
-                print("you may need to toubleshoot each data type look to line 116")
-                department_links.append(department_link)
-
-
-        #debug formating data
-        if debug_main:
-            print("List of Departments:")
-            for Fac in DeptFac_list:
+            # debug formating data
+            if debug_main:
+                print("List of Departments:")
+                for Fac in DeptFac_list:
                     print(Fac)
 
-        # debug formating data
-        if verbose:
-            print("List of Departments & URLS:")
-            for department, link in zip(department_names, department_links):
-                print(f"{department}: {link}")
+            # debug formating data
+            if verbose:
+                print("List of Departments & URLS:")
+                for department, link in zip(department_names, department_links):
+                    print(f"{department}: {link}")
 
-        # Write data to a file to Data File Directory
-        # Ensure the "DataFiles" directory exists
-        data_directory = "DataFiles"
-        os.makedirs(data_directory, exist_ok=True)
+            # Write data to a file to Data File Directory
+            # Ensure the "DataFiles" directory exists
+            data_directory = "DataFiles"
+            os.makedirs(data_directory, exist_ok=True)
 
-        file_path = os.path.join(data_directory, "FacData.txt")
+            file_path = os.path.join(data_directory, "FacData.txt")
 
-        with open(file_path, "w") as file:
-            for dept_fac in DeptFac_list:
-                file.write(f"{dept_fac.dept}:\n")
-                for faculty in dept_fac.fac:
-                    file.write(f"\t{faculty}\n")
+            with open(file_path, "w") as file:
+                for dept_fac in DeptFac_list:
+                    file.write(f"{dept_fac.dept}:\n")
+                    for faculty in dept_fac.fac:
+                        file.write(f"\t{faculty}\n")
 
-        print(f"FacData.txt file saved in {data_directory}. SCRIPT END")
-
-    else:
+        else:
             if debug_main:
                 print("No department list found on the webpage.")
-else:
+    else:
+        if debug_main:
+            print("Failed to retrieve the webpage. Status Code:", response.status_code)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    # timeing stuff
+    minutes = int(execution_time // 60)
+    seconds = execution_time % 60
     if debug_main:
-        print("Failed to retrieve the webpage. Status Code:", response.status_code)
+        print(f"Execution time: {minutes} minutes and {seconds:.2f} seconds")
+    print(f"FacData.txt file saved in {data_directory}. SCRIPT END")
 
 
-#note conecton is ened after data is local
+
+
+if __name__ == "__main__":
+    main()
